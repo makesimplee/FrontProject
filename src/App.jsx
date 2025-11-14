7a9ac7576178b0508ce07c9d9b89aa05cae8805f
makesimplee <vartikad461668@gmail.com>
2025-11-14 15:04:07 +0530

    hello

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+ import {Contact} from "./components/Contact";
+ 
+// import { Footer } from "./components/Footer";
+
+const App=()=>{
+  return(
+    <>
+     
+      
+  <Contact />
+
+
+   </>
+  );
+};
+
+export default App;
